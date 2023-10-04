3b5b0a99aca412213cf33474dc7a39a4e596db4a
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-10-04 09:05:32 +0200

    changed the path to static

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,12 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from .views import index, DepartmentListView, DepartmentUpdateView, DepartmentDeleteView, MedicamentListView, \
-    MedicamentUpdateView, MedicamentDeleteView, RoomListView, AssistantListView, AssistantCreateView, \
-    DepartmentCreateView
+from .views import index
 
 urlpatterns = [
     path("", index, name="index"),
     path('', index),
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "hospital"
